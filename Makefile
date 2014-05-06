2778b7b0ac3192a21450b26806c1bb4191fb1b13
ente <ducksource@duckpond.ch>
2014-05-06 14:16:16 +0200

    first working draft

---
+++ Makefile
@@ -0,0 +1,49 @@
+# Bitscope Makefile
+# root level
+# Mischa Lehmann <ducksource@duckpond.ch>
+# vim: set noexpandtab list:
+
+#Variables
+WORK_DIR := $(shell pwd)
+TOPLEVEL := $(shell git rev-parse --show-toplevel)
+REMOTE_C := $(shell git remote | wc -l)
+MODULES := dso logic chart meter bitscope
+
+#PROGRAMS
+MAKEPKG := makepkg
+
+.PHONY: all
+all : update init pkg
+
+.PHONY: update
+update:
+	if [ $(REMOTE_C) -gt 0 ];then\
+	    git pull;\
+	fi
+
+.PHONY: init
+init:
+	. $(TOPLEVEL)/init.sh $(WORK_DIR) ;\
+	defineVars ;\
+	newlineIFS ;\
+	for tmpl in $$(find $(TOPLEVEL) -name PKGBUILD.template ) ; do \
+	    tmpl_dst="$$(dirname $${tmpl})/PKGBUILD" ;\
+	    cp "$${tmpl}" "$${tmpl_dst}" ;\
+	    replaceVars "$${tmpl_dst}" ;\
+	done ;\
+	restoreIFS ;\
+
+.PHONY: pkg
+pkg: init
+	for module in $(MODULES) ; do \
+	  cd $(TOPLEVEL)/$${module} ;\
+	  $(MAKEPKG) -f -S ;\
+	done
+
+.PHONY: clean 
+clean:
+
+.PHONY: install
+install: pkg
+	$(MAKE) -C pkg install
+
---
+++ init.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+#=== Variable ===s
+# Configuration
+VARIABLE_INDICATOR="__"
+INPUT_STRING_LEN=15
+OUTPUT_VARNAME_LEN=15
+OUTPUT_STRING_LEN=20
+
+#Used files/directories
+DIR_CALLDIR="$(pwd)"
+DIR_ARG="${1}/"
+VARIABLE_CONFIG_FILE="${DIR_ARG}/vars.sh"
+
+#Metadata
+VARIABLE_DECLARATION=""
+
+#Intial tools
+INIT_TOOLS="${DIR_ARG}/tools/"
+AWESOMEREPLACE="${INIT_TOOLS}/awesomereplace"
+SPONGE="${INIT_TOOLS}/sponge"
+
+#=== Functions ===
+function newlineIFS(){
+    OLDIFS="${IFS}"
+    IFS=$'\n'
+}
+
+function semicolonIFS(){
+    OLDIFS="${IFS}"
+    IFS=';'
+}
+
+function restoreIFS(){
+    IFS="${OLDIFS}"
+}
+
+function inputFixedString(){
+    local readstring=""
+    while [ "${readstring}" == "" ]; do
+        read -r readstring
+    done
+    printf "%${INPUT_STRING_LEN}.${INPUT_STRING_LEN}s" "${readstring}"
+}
+
+function printVars(){
+    semicolonIFS
+    for varname in ${VARIABLE_DECLARATION}; do
+        local varplaceholder="${VARIABLE_INDICATOR}${varname}${VARIABLE_INDICATOR}"
+        local vardata="${!varname}"
+        printf "%${OUTPUT_VARNAME_LEN}.${OUTPUT_VARNAME_LEN}s : %${OUTPUT_VARNAME_LEN}.${OUTPUT_VARNAME_LEN}s : %s\n" "${varname}" "${varplaceholder}" "${vardata}"
+    done
+    restoreIFS
+}
+
+function defineVars(){
+    echo "=== Defining variables ==="
+    echo "Please define variables in: ${VARIABLE_CONFIG_FILE}"
+    echo "[ENTER]"
+    #read
+    #vim "${VARIABLE_CONFIG_FILE}"
+
+    echo "=== Sourcing variables file ==="
+    source "${VARIABLE_CONFIG_FILE}"
+
+    echo "=== Load variabels ==="
+    newlineIFS
+    VARIABLE_DECLARATION=""
+    for var in $(set); do
+        varname=$(echo "${var}" | sed -nre 's/^([a-zA-Z0-9]+)\=.+$/\1/p')
+        if [ "${varname}" != "" ]; then
+            VARIABLE_DECLARATION="${varname};${VARIABLE_DECLARATION}"
+        fi
+    done
+    restoreIFS
+
+    echo "=== Variables ==="
+    printVars
+}
+
+function replaceVars(){
+    local file="${1}"
+    if [ -f "${file}" ]; then
+        semicolonIFS
+        for varname in ${VARIABLE_DECLARATION}; do
+            local varplaceholder="${VARIABLE_INDICATOR}${varname}${VARIABLE_INDICATOR}"
+            local vardata="${!varname}"
+            cat "${file}" | "${AWESOMEREPLACE}" "${varplaceholder}" "${vardata}" | "${SPONGE}" "${file}"
+        done
+        restoreIFS
+    fi
+}
